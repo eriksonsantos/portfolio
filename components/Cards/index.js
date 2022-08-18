a01691392e2fe20e5c311098d8b71a1ecd771f39
eriksonsantos <eriksoncode@gmail.com>
2022-08-18 07:07:25 -0300

    resize cards

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -4,7 +4,7 @@ import styles from './Cards.module.scss'
 export default function CardsPortfolio({ props }) {
   return (
     <div className={styles.Container}>
-      <SonyCards className={styles.Cards} style={{ width: '20rem' }}>
+      <SonyCards className={styles.Cards}>
         <SonyImgCards variant="top" src={props.image} />
         <SonyBodyCards>
           <SonyTitleCards >{props.title}</SonyTitleCards>
